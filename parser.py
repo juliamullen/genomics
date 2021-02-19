052f8b7af4b8dc35397d1bb67bf23bc3a6ef5225
Julia Mullen <julia.l.mullen@gmail.com>
2021-02-19 01:52:03 -0500

    remove testing setup

--- parser.py
+++ parser.py
@@ -2,8 +2,8 @@ import json
 import gzip
 import os
 
-#from biothings import config
-#logger = config.logger
+from biothings import config
+logger = config.logger
 
 def load_annotations(data_folder):
     json_path = os.path.join(data_folder, "api_data.json.gz")
@@ -12,6 +12,3 @@ def load_annotations(data_folder):
     for datum in data:
         datum['_id'] = datum['accession_id']
         yield datum
-
-
-print([i for i in load_annotations('/Users/julia/Downloads')])
