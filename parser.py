525941071c66ba5db3f85d0ee7817eaeb74f8fea
Julia Mullen <julia.l.mullen@gmail.com>
2021-02-19 01:51:00 -0500

    _ids

--- parser.py
+++ parser.py
@@ -2,11 +2,16 @@ import json
 import gzip
 import os
 
-from biothings import config
-logger = config.logger
+#from biothings import config
+#logger = config.logger
 
 def load_annotations(data_folder):
     json_path = os.path.join(data_folder, "api_data.json.gz")
     with gzip.open(json_path) as f:
         data = json.loads(f.read().decode('utf-8'))
-    yield from (i for i in data)
+    for datum in data:
+        datum['_id'] = datum['accession_id']
+        yield datum
+
+
+print([i for i in load_annotations('/Users/julia/Downloads')])
