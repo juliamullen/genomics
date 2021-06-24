7902cd9db6f4b0442d2658dfd4c4a00197594360
Julia Mullen <julia.l.mullen@gmail.com>
2021-06-24 11:10:47 -0400

    dont need to decode

--- parser.py
+++ parser.py
@@ -9,6 +9,6 @@ def load_annotations(data_folder):
     json_path = os.path.join(data_folder, "api_data_2021-06-23-00-16.json.gz")
     with gzip.open(json_path) as f:
         for line in f:
-            datum = json.loads(line.decode('utf-8'))
+            datum = json.loads(line)
             datum['_id'] = datum['accession_id']
             yield datum
