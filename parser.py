711f0dde94898dbb934295fb5802b02d202579bf
Julia Mullen <julia.l.mullen@gmail.com>
2021-07-21 12:30:59 -0400

    upstream data fix

--- parser.py
+++ parser.py
@@ -6,7 +6,7 @@ from biothings import config
 logger = config.logger
 
 def load_annotations(data_folder):
-    json_path = os.path.join(data_folder, "api_data_2021-06-23-00-16.json.gz")
+    json_path = os.path.join(data_folder, "new_api_data_fix.json.gz")
     with gzip.open(json_path) as f:
         for line in f:
             datum = json.loads(line)
