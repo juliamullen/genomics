2a5bf69d9fcbd8f65f2f697e0e9eebf854bee92e
Everaldo <everaldorodrigo@gmail.com>
2023-12-04 10:15:16 -0800

    Add data source to muts and mutless. (#2)
    * Add data source to muts and mutless.
    
    * Change uploader name, fix _id, and add crumbs.

--- mapping.py
+++ mapping.py
@@ -1,113 +1,90 @@
-def get_customized_mapping(cls):
+def mapping_data_mutless(cls):
     return {
-            "mutations": {
-                "type": "nested",
-                "properties": {
-                    "type": {
-                        "type": "keyword"
-                        },
-                    "mutation": {
-                        "type": "keyword",
-                        "normalizer": "keyword_lowercase_normalizer"
-                        },
-                    "gene": {
-                        "type": "keyword"
-                        },
-                    "ref_codon": {
-                        "type": "keyword"
-                        },
-                    "pos": {
-                        "type": "keyword"
-                        },
-                    "alt_codon": {
-                        "type": "keyword"
-                        },
-                    "is_synonymous": {
-                        "type": "keyword"
-                        },
-                    "ref_aa": {
-                        "type": "keyword"
-                        },
-                    "codon_num": {
-                        "type": "keyword"
-                        },
-                    "alt_aa": {
-                        "type": "keyword"
-                        },
-                    "absolute_coords": {
-                        "type": "keyword"
-                        },
-                    "change_length_nt": {
-                        "type": "keyword"
-                        },
-                    "nt_map_coords": {
-                        "type": "keyword"
-                        },
-                    "aa_map_coords": {
-                        "type": "keyword"
-                        }
-                    }
-                },
-            "division": {
-                "type": "keyword",
-                },
-            "division_lower": {
-                "type": "keyword",
-                "normalizer": "keyword_lowercase_normalizer"
-
-                },
+        "mutations": {
+            "type": "keyword",
+            "normalizer": "keyword_lowercase_normalizer"
+        },
+        "division": {
+            "type": "keyword",
+        },
+        "division_lower": {
+            "type": "keyword",
+            "normalizer": "keyword_lowercase_normalizer"
+        },
         "country": {
-                "type": "keyword",
-                },
+            "type": "keyword",
+        },
         "country_lower": {
-                "type": "keyword",
-                "normalizer": "keyword_lowercase_normalizer"
-
-                },
+            "type": "keyword",
+            "normalizer": "keyword_lowercase_normalizer"
+        },
         "date_submitted": {
-                "type": "keyword"
-                },
+            "type": "keyword"
+        },
         "date_collected": {
-                "type": "keyword"
-                },
+            "type": "keyword"
+        },
         "date_modified": {
-                "type": "keyword"
-                },
+            "type": "keyword"
+        },
         "country_id": {
-                "type": "keyword"
-                },
-        "authors": {
-                "type": "keyword"
-                },
+            "type": "keyword"
+        },
         "pangolin_lineage": {
-                "type": "keyword",
-                "normalizer": "keyword_lowercase_normalizer"
-
-                },
+            "type": "keyword",
+            "normalizer": "keyword_lowercase_normalizer"
+        },
+        "pangolin_lineage_crumbs": {
+            "type": "keyword",
+            "normalizer": "keyword_lowercase_normalizer"
+        },
         "location": {
-                "type": "keyword"
-                },
+            "type": "keyword"
+        },
         "location_lower": {
-                "type": "keyword",
-                "normalizer": "keyword_lowercase_normalizer"
-                },
+            "type": "keyword",
+            "normalizer": "keyword_lowercase_normalizer"
+        },
         "location_id": {
-                "type": "keyword"
-                },
+            "type": "keyword"
+        },
         "division_id": {
-                "type": "keyword"
-                },
+            "type": "keyword"
+        },
         "accession_id": {
-                "type": "keyword"
-                },
-        "clade": {
-                "type": "keyword"
-                },
-        "pango_version": {
-                "type": "keyword"
-                },
-        "country_normed": {
-                "type": "keyword"
-                }
+            "type": "keyword"
+        }
+    }
 
-}
+
+def mapping_data_muts(cls):
+    return {
+        "type": {
+            "type": "keyword"
+        },
+        "mutation": {
+            "type": "keyword",
+            "normalizer": "keyword_lowercase_normalizer"
+        },
+        "gene": {
+            "type": "keyword"
+        },
+        "ref_codon": {
+            "type": "keyword"
+        },
+        "alt_codon": {
+            "type": "keyword"
+        },
+        "is_synonymous": {
+            "type": "keyword"
+        },
+        "ref_aa": {
+            "type": "keyword"
+        },
+        "codon_num": {
+            "type": "keyword"
+        },
+        "alt_aa": {
+            "type": "keyword"
+        }
+    }
--- parser.py
+++ parser.py
@@ -17,10 +17,18 @@ def update_not_smaller(new_data_file):
     if old_num_lines > new_num_lines:
         raise RuntimeError(f"Attempted update with fewer documents failed. Current: {old_num_lines} New: {new_num_lines}")
 
-def load_annotations(data_folder):
-    json_path = os.path.join(data_folder, "new_api_data.json.gz")
+def load_data_muts(data_folder):
+    json_path = os.path.join(data_folder, "data.muts.jsonl.gz")
     with gzip.open(json_path) as f:
         for line in f:
             datum = json.loads(line)
-            datum['_id'] = datum['accession_id']
+            datum['_id'] = datum['id']
+            yield datum
+
+def load_data_mutless(data_folder):
+    json_path = os.path.join(data_folder, "data.mutless.jsonl.gz")
+    with gzip.open(json_path) as f:
+        for line in f:
+            datum = json.loads(line)
+            datum['_id'] = str(datum['accession_id'])
             yield datum
